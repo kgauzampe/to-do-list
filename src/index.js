1ad40ca5d8aac61c3e349c8b4dfd024bf8a48209
kgauzampe <kgauza.mpe@gmail.com>
2022-01-08 11:08:15 +0200

    files added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as firebase from '@firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpy8zDKh25stxyhIDc6JZ_DJ7zQvwNdgY",
+  authDomain: "note-app-d5e86.firebaseapp.com",
+  databaseURL: "https://note-app-d5e86.firebaseio.com",
+  projectId: "note-app-d5e86",
+  storageBucket: "note-app-d5e86.appspot.com",
+  messagingSenderId: "524690983895",
+  appId: "1:524690983895:web:ccb9f7e5c70e695628d992"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +24,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
